fca083d098366bbf6c2fd640bd8af879099c9142
nghiapro123z <nghiatmgch17633@fpt.edu.vn>
2019-07-31 11:23:48 +0700

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+       *{
+           box-sizing: border-box;
+       }
+        img{
+            display: block;
+            max-width: 100%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        h3{
+            text-align: center;
+        }
+    </style>
+    <script>
+
+    </script>
+</head>
+<body>
+<header>
+    <nav>
+
+    </nav>
+</header>
+<div>
+    <img src="https://i.pximg.net/img-original/img/2019/07/30/23/22/21/75923538_p0.png" alt="PHOTO">
+</div>
+<div>
+    <h3>Rem</h3>
+    <h3>Love Subaru !</h3>
+</div>
+<fooster>
+
+</fooster>
+</body>
+</html>
